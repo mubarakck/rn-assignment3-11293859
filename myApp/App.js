4918d5b0f2784866ceeb7729ec7e0b6fd61d4ff8
Mubarak Wumpini Abdul-Rashid <mwabdul-rashid@st.ug.edu.gh>
2024-06-02 18:46:22 +0000

    added all the categories section

--- myApp/App.js
+++ myApp/App.js
@@ -24,11 +24,79 @@ export default function App() {
         <Image source={ require("./assets/more.png")}  style={styles.filterIcon} />
       </View>
 
+
+
       <Text style={styles.sectionTitle}>Categories</Text>
 
 
 
+      <View style={styles.categories}>
 
+
+      <View style={styles.category}>         
+          <Text style={styles.categoryText}>Exercise</Text>
+          <Text style={styles.categoryTaskCount}>12 Tasks</Text>
+          <Image source={ require("./assets/img1.png")} style={styles.categoryImage} />
+        </View>
+
+        <View style={styles.category}>        
+          <Text style={styles.categoryText}>Study</Text>
+        <Text style={styles.categoryTaskCount}>12 Tasks</Text>
+        <Image source={ require("./assets/img2.png")} style={styles.categoryImage} />
+        </View>
+
+        <View style={styles.category}>         
+          <Text style={styles.categoryText}>Wash</Text>
+          <Text style={styles.categoryTaskCount}>12 Tasks</Text>
+          <Image source={ require("./assets/wash.png")} style={styles.categoryImage} />
+        </View>
+
+        <View style={styles.category}>         
+          <Text style={styles.categoryText}>Exercise</Text>
+          <Text style={styles.categoryTaskCount}>12 Tasks</Text>
+          <Image source={ require("./assets/exercise.png")} style={styles.categoryImage} />
+        </View>
+
+        <View style={styles.category}>         
+          <Text style={styles.categoryText}>Study</Text>
+          <Text style={styles.categoryTaskCount}>12 Tasks</Text>
+          <Image source={ require("./assets/study.png")} style={styles.categoryImage} />
+        </View>
+
+        <View style={styles.category}>         
+          <Text style={styles.categoryText}>Code</Text>
+          <Text style={styles.categoryTaskCount}>12 Tasks</Text>
+          <Image source={ require("./assets/code.png")} style={styles.categoryImage} />
+        </View>
+
+        <View style={styles.category}>         
+          <Text style={styles.categoryText}>Cook</Text>
+          <Text style={styles.categoryTaskCount}>12 Tasks</Text>
+          <Image source={ require("./assets/cook.png")} style={styles.categoryImage} />
+        </View>
+
+        <View style={styles.category}>         
+          <Text style={styles.categoryText}>Dance</Text>
+          <Text style={styles.categoryTaskCount}>12 Tasks</Text>
+          <Image source={ require("./assets/dance.png")} style={styles.categoryImage} />
+        </View>
+
+        <View style={styles.category}>         
+          <Text style={styles.categoryText}>Eat</Text>
+          <Text style={styles.categoryTaskCount}>12 Tasks</Text>
+          <Image source={ require("./assets/eat.png")} style={styles.categoryImage} />
+        </View>
+
+        
+	        <View style={styles.category}>         
+          <Text style={styles.categoryText}>Bath</Text>
+          <Text style={styles.categoryTaskCount}>12 Tasks</Text>
+          <Image source={ require("./assets/bath.png")} style={styles.categoryImage} />
+        </View>
+
+        
+
+        </View>
 
 
 
@@ -114,13 +182,31 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 10,
   },
-  categories: {
-    flexDirection: 'row',
-  },
   sectionTitle: {
     fontSize: 18,
     fontWeight: 'bold',
     marginVertical: 10,
+  },
+  categories: {
+    flexDirection: 'row',
+  },
+  category: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 10,
+    alignItems: 'center',
+    width: '48%',
+    marginRight: 30,
+  },
+  categoryImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+  },
+  categoryText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 10,
   },
   
 });
