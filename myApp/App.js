7d98b8ec5256a0ed5ea3a49c752d31f0dd47020c
Mubarak Wumpini Abdul-Rashid <mwabdul-rashid@st.ug.edu.gh>
2024-06-02 18:34:56 +0000

    created and edited the header and task count section

--- myApp/App.js
+++ myApp/App.js
@@ -4,11 +4,70 @@ import { StyleSheet,ScrollView, TextInput, Image,  Text, View } from 'react-nati
 export default function App() {
   return (
     <ScrollView style={styles.container}>
+
+
+
+
+
+
+
       <View style={styles.header}>
       <Text style={styles.greeting}>Hello, Devs</Text>
-      <Text style={styles.taskCount}>14 tasks today</Text>
-      <StatusBar style="auto" />
+      <Image source={ require("./assets/person.png")}/>
     </View>
+
+
+    <Text style={styles.taskCount}>14 tasks today</Text>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </ScrollView>
   );
 }
@@ -16,8 +75,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F7F0E8;   ',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor:'#F5EDE3',
+    padding: 20, 
   },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  greeting: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  taskCount: {
+    fontSize: 16,
+    color: '#888',
+  },
+  
 });
